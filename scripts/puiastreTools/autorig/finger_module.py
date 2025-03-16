11148bdd5b8b0d3849495695865e0317e61bfab7
GuiidoGC <guidogoca@gmail.com>
2025-03-16 18:55:48 +0100

    Refactor FingerModule to implement FK controllers and bendy finger functionality; add NictitantMembran class for joint projection on surface

--- scripts/puiastreTools/autorig/finger_module.py
+++ scripts/puiastreTools/autorig/finger_module.py
@@ -8,89 +8,113 @@ class FingerModule(object):
         self.finger_chain.append(self.main_finger)
         self.finger_chain.reverse()
         self.side = self.finger_chain[0].split("_")[0]
-        self.name = self.finger_chain[0].split("_")[1]
 
-        self.pairblends()
-        self.add_controllers()
-
-    def pairblends(self):
-
-        
-        count = 0
-        for name in ["GRP", "SPC", "OFF", "SDK", "ANM", "CTL"]:
-            if cmds.ls(f"{self.side}_settings{self.name}_{name}"):
-                count += 1
-
-        if count == 0:
-            self.settings_ctl, self.settings_grp = ctls_cre.controller_creator(f"{self.side}_settings{self.name}")
-            cmds.matchTransform(self.settings_grp[0], self.finger_chain[0])
-            cmds.move(0, 100, 0, self.settings_grp[0], r=True, worldSpace=True)
-            for attr in ["tx", "ty", "tz", "rx", "ry", "rz", "sx", "sy", "sz", "visibility"]:
-                cmds.setAttr(f"{self.settings_ctl}.{attr}", lock=True, keyable=False, channelBox=False)
-
-            cmds.addAttr(self.settings_ctl, shortName="switchIkFk", niceName="Switch IK --> FK", maxValue=1, minValue=0,defaultValue=1, keyable=True)
-            cmds.addAttr(self.settings_ctl, shortName="curvatureSep", niceName="CURVATURE_____", enumName="_____",attributeType="enum", keyable=True)
-            cmds.addAttr(self.settings_ctl, shortName="curvature", niceName="Curvature", maxValue=1, minValue=0,defaultValue=0, keyable=True)
-            cmds.setAttr(self.settings_ctl+".curvatureSep", channelBox=True, lock=True)              
-
-        else:
-            self.settings_ctl = f"{self.side}settings_ctl"
-            self.settings_grp = [f"{self.side}_settings_{name}" for name in ["GRP", "SPC", "OFF", "SDK", "ANM"]]
-
-        
-
-        self.ik_chain = []
-        self.fk_chain = []
-        for i, name in enumerate(["Fk", "Ik"]):
-            dupe = cmds.duplicate(self.finger_chain, n=name)
-
-            for j, joint in enumerate(dupe):
-                self.name = self.finger_chain[j].split("_")[1]
-                end_joint = cmds.rename(joint, self.side + "_" + self.name + name + "_JNT")
-
-                if name == "Ik":
-                    self.ik_chain.append(end_joint)
-                elif name == "Fk":
-                    self.fk_chain.append(end_joint)
-
-        for i, joint in enumerate(self.ik_chain):
-            pairblend = cmds.createNode("pairBlend", n=joint.replace("Ik_JNT", "_PBL"))
-            cmds.connectAttr(joint + ".translate", pairblend + ".inTranslate1")
-            cmds.connectAttr(joint + ".rotate", pairblend + ".inRotate1")
-            cmds.connectAttr(self.fk_chain[i] + ".translate", pairblend + ".inTranslate2")
-            cmds.connectAttr(self.fk_chain[i] + ".rotate", pairblend + ".inRotate2")
-            cmds.connectAttr(pairblend + ".outTranslate", self.finger_chain[i] + ".translate")
-            cmds.connectAttr(pairblend + ".outRotate", self.finger_chain[i] + ".rotate")
-            cmds.connectAttr(self.settings_ctl + ".switchIkFk", pairblend + ".weight")
-
-        self.ik_handle = cmds.ikHandle(sj=self.ik_chain[0], ee=self.ik_chain[-1], n=f"{self.side}_{self.name}_HDL", solver="ikRPsolver")
-        
+        self.fk_controllers()
+        self.bendy_finger()
 
 
-    def add_controllers(self):
-        self.fk_ctls = []
-        self.fk_ctls_grp = []
+    def fk_controllers(self):
 
-        for fk_joint in self.fk_chain:
-            ctl, grp = ctls_cre.controller_creator(fk_joint.replace("_JNT", ""))
-            cmds.matchTransform(grp[0], fk_joint)
-
-            if self.fk_ctls:
-                cmds.parent(grp[0], self.fk_ctls[-1])
-            self.fk_ctls.append(ctl)
-            self.fk_ctls_grp.append(grp)
-
-            cmds.parentConstraint(ctl, fk_joint) ##### CANVIAR QUAN LA LAIA ACABI MATRIUS
-
-
-        self.ik_ctls = []
-        self.ik_ctls_grp = []
-
-        for i, names in enumerate([self.name, f"{self.name}Pv", f"{self.name}Ik"]):
-            ctl, grp = ctls_cre.controller_creator(f"{self.side}_{names}")
-            cmds.matchTransform(grp[0], self.ik_chain[])
+        self.ctls = []
+        self.blend_joints = []
+        for i, jnt in enumerate(self.finger_chain):
+            name = jnt.split("_")[1]
+            ctl = ctls_cre.controller_creator(name=f"{self.side}_{name}")
+            cmds.matchTransform(ctl[1][0], jnt)
 
             
+            cmds.select(clear=True)
+            joint = cmds.joint(name = f"{self.side}_{name}Blend_JNT")
+
+            cmds.connectAttr(ctl[0] + ".worldMatrix[0]", joint + ".offsetParentMatrix")
+
+            if self.ctls:
+                cmds.parent(ctl[1][0], self.ctls[-1][0])
+
+            self.ctls.append(ctl)
+            self.blend_joints.append(joint)
+        
+    
+    def bendy_finger(self):
+       
+        positions = [cmds.xform(jnt, q=True, ws=True, t=True) for jnt in self.blend_joints]
+        linear_curve = cmds.curve(name=f"{self.side}_linear_CRV", d=1, p=positions)
+
+        detached01 = cmds.detachCurve( f'{linear_curve}.u[{1}]', ch=True, replaceOriginal=False)
+        detached02 = cmds.detachCurve( f'{detached01[1]}.u[{2}]', ch=True, replaceOriginal=True)
+
+        for cv in range(len(positions)):
+            name = self.blend_joints[cv].split("_")[1]
+            dcpm = cmds.createNode("decomposeMatrix", name=f"{self.side}_{name}0{cv+1}_DCM")
+            cmds.connectAttr(f"{self.blend_joints[cv]}.worldMatrix[0]", f"{dcpm}.inputMatrix")
+            cmds.connectAttr(f"{dcpm}.outputTranslate", f"{linear_curve}.controlPoints[{cv}]")
+
+        self.bendy_curves = []
+        for i, curve in enumerate([detached01[0], detached01[1], detached02[0]]):
+            name = self.blend_joints[i].split("_")[1]
+            renamed = cmds.rename(curve, f"{self.side}_{self.blend_joints[i].split('_')[1]}0_CRV")
+
+            cmds.select(clear=True)
+            bendy_pos_joint = cmds.joint(name=f"{self.side}_{name}BendyPos_JNT")
+            wtAdd = cmds.createNode("wtAddMatrix", name=f"{self.side}_{name}Bendy_WAM", ss=True)
+            cmds.connectAttr(f"{self.blend_joints[i]}.worldMatrix[0]", f"{wtAdd}.wtMatrix[0].matrixIn")
+            cmds.connectAttr(f"{self.blend_joints[i+1]}.worldMatrix[0]", f"{wtAdd}.wtMatrix[1].matrixIn")
+
+            pickMatrix = cmds.createNode("pickMatrix", name=f"{self.side}_{name}Bendy_PM", ss=True)
+            cmds.connectAttr(f"{wtAdd}.matrixSum", f"{pickMatrix}.inputMatrix")
+            cmds.setAttr(f"{wtAdd}.wtMatrix[0].weightIn", 0.5)
+            cmds.setAttr(f"{wtAdd}.wtMatrix[1].weightIn", 0.5)
+            for attr in ["useRotate", "useScale", "useShear"]:
+                cmds.setAttr(f"{pickMatrix}.{attr}", 0)
+
+            aim_matrix = cmds.createNode("aimMatrix", name=f"{self.side}_{name}Bendy_AIM", ss=True)
+            cmds.connectAttr(f"{pickMatrix}.outputMatrix", aim_matrix + ".inputMatrix")
+            cmds.connectAttr(f"{self.blend_joints[i+1]}.worldMatrix[0]", aim_matrix + ".primaryTargetMatrix")
+            cmds.setAttr(f"{aim_matrix}.primaryInputAxis", 1, 0, 0)
+
+            cmds.connectAttr(aim_matrix + ".outputMatrix", bendy_pos_joint + ".offsetParentMatrix")
+        
+            cmds.rebuildCurve(renamed, ch=True, rpo=True, rt=0, end=True, kr=False, kcp=False, kep=True, kt=True, fr=False, s=2, d=1, tol=0.01)
+            cmds.delete(renamed, ch=True)
+
+            cmds.select(renamed, r=True)
+            cmds.nurbsCurveToBezier()
+            cmds.select(renamed + ".cv[0]", renamed + ".cv[1]", renamed + ".cv[5]", renamed + ".cv[6]", r=True)
+            cmds.bezierAnchorPreset(preset=2)
+            cmds.select(renamed + ".cv[2]", renamed + ".cv[3]", renamed + ".cv[4]", r=True)
+            cmds.bezierAnchorPreset(preset=1)
+            cmds.bezierAnchorState(smooth=True, even=True)
+
+            self.bendy_curves.append(renamed)
+
+            bendy_joints = []
+            for names in ["Root", "Mid", "Hook"]:
+                cmds.select(clear=True)
+                bendy_joints.append(cmds.joint(name=f"{self.side}_{name}{names}_JNT"))
+
+
+            bendy_ctl = ctls_cre.controller_creator(name=f"{self.side}_{name}Bendy")
+            cmds.connectAttr(bendy_pos_joint + ".worldMatrix[0]", bendy_ctl[1][0] + ".offsetParentMatrix")
+
+            cmds.connectAttr(f"{self.blend_joints[i]}.worldMatrix[0]", bendy_joints[0] + ".offsetParentMatrix")
+            cmds.connectAttr(f"{self.blend_joints[i+1]}.worldMatrix[0]", bendy_joints[2] + ".offsetParentMatrix")
+            cmds.connectAttr(bendy_ctl[0] + ".worldMatrix[0]", bendy_joints[1] + ".offsetParentMatrix")
+
+            skin = cmds.skinCluster(bendy_joints, renamed, n=f"{self.side}_{name}_SKN")[0]    
+
+            cmds.skinPercent(skin, renamed + ".cv[2]", tv=(bendy_joints[1], 1))
+            cmds.skinPercent(skin, renamed + ".cv[4]", tv=(bendy_joints[1], 1))
+
+            off_curve = cmds.offsetCurve(renamed, ch=True, rn=False, cb=2, st=True, cl=True, cr=0, d=50, tol=0.01, sd=0, ugn=False, name=f"{self.side}_{name}Offset_CRV")
+
+            cmds.setAttr(f"{off_curve[1]}.useGivenNormal", 1)
+            cmds.setAttr(f"{off_curve[1]}.normal", 0,0,1)
+
+            cmds.setAttr(off_curve[1] + ".normal", 1, 0 , 0)
+
+            # for i in range(5):
+
+
 
 
 
