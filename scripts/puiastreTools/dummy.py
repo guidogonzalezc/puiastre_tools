fc8b954fa181323d99c7b76fafcf0a6f38451a5a
GuiidoGC <guidogoca@gmail.com>
2025-09-27 12:41:47 +0200

    script per canviar rapidament els overrides dels controladors

--- scripts/puiastreTools/dummy.py
+++ scripts/puiastreTools/dummy.py
@@ -1,18 +1,7 @@
 import maya.cmds as cmds
 
-for item in cmds.ls():
+for item in cmds.ls(sl=True):
     if cmds.nodeType(item) == "transform":
-        if "L_" in item and "_CTL" in item:
-            if "Bendy" in item:
-                cmds.setAttr(item + ".overrideEnabled", 1)
-                cmds.setAttr(item + ".overrideColor", 18)  # Pink for Left Bendy
-            else:
-                cmds.setAttr(item + ".overrideEnabled", 1)
-                cmds.setAttr(item + ".overrideColor", 6)  # Blue for Left side
-        elif "R_" in item and "_CTL" in item:
-            if "Bendy" in item:
-                cmds.setAttr(item + ".overrideEnabled", 1)
-                cmds.setAttr(item + ".overrideColor", 4)  # Light Blue for Right Bendy
-            else:
-                cmds.setAttr(item + ".overrideEnabled", 1)
-                cmds.setAttr(item + ".overrideColor", 13)  # Red for Right side
+        if "_CTL" in item:
+            cmds.setAttr(item + ".overrideEnabled", 1)
+            cmds.setAttr(item + ".overrideColor", 13)  # Pink for Left Bendy
